99d7b8767cc085e0e6f9259aeb88b4ce9ee1ffa8
Doug Bell <madcityzen@gmail.com>
2015-01-19 23:03:46 -0600

    add a file deploy for deploying to the filesystem
    This is the analogue of the File store, but for deploying. Mostly I want
    this for testing purposes, but it's also a good thing to subclass for
    any Deploys that need to copy files locally.

---
+++ lib/Statocles/Deploy/File.pm
@@ -0,0 +1,64 @@
+package Statocles::Deploy::File;
+# ABSTRACT: Deploy a site to a folder on the filesystem
+
+use Statocles::Base 'Class';
+with 'Statocles::Deploy';
+
+=attr path
+
+The path to deploy to.
+
+=cut
+
+has path => (
+    is => 'ro',
+    isa => Dir,
+    coerce => Dir->coercion,
+    required => 1,
+);
+
+=method deploy( FROM_STORE, MESSAGE )
+
+Deploy the site, copying from the given store.
+
+=cut
+
+sub deploy {
+    my ( $self, $from_store, $message ) = @_;
+
+    my @files;
+    my $iter = $from_store->find_files;
+    while ( my $path = $iter->() ) {
+        # Git versions before 1.7.4.1 require a relative path to 'git add'
+        push @files, $path->relative( "/" )->stringify;
+
+        # XXX Implement a friendlier way to copy files from Stores
+        my $in_fh = $from_store->open_file( $path );
+        my $out_fh = $self->path->child( $path )->openw_raw;
+        while ( my $line = <$in_fh> ) {
+            $out_fh->print( $line );
+        }
+    }
+
+    return @files;
+}
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This class allows a site to be deployed to a folder on the filesystem.
+
+This class extends L<Statocles::Store::File|Statocles::Store::File>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Statocles::Store::File>
+
+=item L<Statocles::Deploy>
+
+=back
+
---
+++ t/deploy/file.t
@@ -0,0 +1,35 @@
+
+use Statocles::Base 'Test';
+use Statocles::Deploy::File;
+
+my $SHARE_DIR = path( __DIR__ )->parent->child( 'share' );
+
+my @temp_args;
+if ( $ENV{ NO_CLEANUP } ) {
+    @temp_args = ( CLEANUP => 0 );
+}
+
+my $build_store = Statocles::Store::File->new(
+    path => $SHARE_DIR->child( qw( deploy ) ),
+);
+
+
+subtest 'deploy' => sub {
+    my $tmpdir = tempdir( @temp_args );
+    diag "TMP: " . $tmpdir if @temp_args;
+
+    my $deploy = Statocles::Deploy::File->new(
+        path => $tmpdir,
+    );
+    $deploy->deploy( $build_store );
+
+    subtest 'files are correct' => sub {
+        my $file_iter = $build_store->find_files;
+        while ( my $file = $file_iter->() ) {
+            ok $tmpdir->child( $file->path )->exists,
+                'page ' . $file->path . ' is in deploy path';
+        }
+    };
+};
+
+done_testing;
