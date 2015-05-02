01a37f01d2d314488f47ae24fb0217a8c1c0a67a
Doug Bell <madcityzen@gmail.com>
2015-05-01 23:52:00 -0500

    allow using file path when copying files
    This is step one in trying to reduce the number of open filehandles we
    need.
    
    Refs #226

--- lib/Statocles/Page/File.pm
+++ lib/Statocles/Page/File.pm
@@ -4,16 +4,27 @@ package Statocles::Page::File;
 use Statocles::Base 'Class';
 with 'Statocles::Page';
 
+=attr file_path
+
+The path to the file.
+
+=cut
+
+has file_path => (
+    is => 'ro',
+    isa => Path,
+    coerce => Path->coercion,
+);
+
 =attr fh
 
-The file handle containing the contents of the page. Required.
+The file handle containing the contents of the page.
 
 =cut
 
 has fh => (
     is => 'ro',
     isa => FileHandle,
-    required => 1,
 );
 
 =method vars
@@ -34,7 +45,7 @@ Return
 
 sub render {
     my ( $self ) = @_;
-    return $self->fh;
+    return $self->file_path ? $self->file_path->openr_utf8 : $self->fh;
 }
 
 1;
@@ -42,8 +53,14 @@ __END__
 
 =head1 SYNOPSIS
 
-    open my $fh, '<', '/path/to/file.txt';
+    # File path
+    my $page = Statocles::Page::File->new(
+        path => '/path/to/page.txt',
+        file_path => '/path/to/file.txt',
+    );
 
+    # Filehandle
+    open my $fh, '<', '/path/to/file.txt';
     my $page = Statocles::Page::File->new(
         path => '/path/to/page.txt',
         fh => $fh,
--- t/page/file.t
+++ t/page/file.t
@@ -6,13 +6,10 @@ use Statocles::Page::File;
 my $site = Statocles::Site->new( deploy => tempdir );
 
 subtest 'constructor' => sub {
-    open my $fh, '<', \'string literal';
-
     test_constructor(
         'Statocles::Page::File',
         required => {
             path => '/index.html',
-            fh => $fh,
         },
         default => {
             search_change_frequency => 'weekly',
@@ -24,7 +21,20 @@ subtest 'constructor' => sub {
     );
 };
 
-subtest 'render' => sub {
+subtest 'file path' => sub {
+    my $page = Statocles::Page::File->new(
+        path => '/path/to/page.html',
+        file_path => $SHARE_DIR->child( qw( store files text.txt ) ),
+    );
+
+    my $got_fh = $page->render;
+    is ref $got_fh, 'GLOB', 'got a filehandle';
+
+    my $got_content = do { local $/; <$got_fh> };
+    is $got_content, $SHARE_DIR->child( qw( store files text.txt ) )->slurp_utf8;
+};
+
+subtest 'fh' => sub {
     open my $expect_fh, '<', \'string literal';
 
     my $page = Statocles::Page::File->new(
