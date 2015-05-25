preaction/Statocles

commit 32bfcd86e406417a077339ac71b1c6772bd30257

move url_root into Statocles::App
All apps must have a URL root. Statocles::App is now a role, and will
automatically apply your url_root.

To make url_root more automatic, there are new url() and link() methods
for apps. These methods will automatically add the url_root when needed.

Fixes #244

lib/Statocles/App.pm
@@ -1,8 +1,9 @@
 package Statocles::App;
-# ABSTRACT: Base class for Statocles applications
+# ABSTRACT: Base role for Statocles applications
 
-use Statocles::Base 'Class';
-
+use Statocles::Base 'Role';
+use Statocles::Link;
+requires 'pages';
 
 =attr site
 
@@ -28,6 +29,69 @@ has data => (
     isa => HashRef,
     default => sub { {} },
 );
+
+=attr url_root
+
+The URL root of this application. All pages from this app will be under this
+root. Use this to ensure two apps do not try to write the same path.
+
+=cut
+
+has url_root => (
+    is => 'ro',
+    isa => Str,
+    required => 1,
+);
+
+=method pages()
+
+Get the pages for this app. Must return a list of L<Statocles::Page> objects, with
+the index page (the page intended to be the entry page) first.
+
+=cut
+
+around pages => sub {
+    my ( $orig, $self, @args ) = @_;
+    my @pages = $self->$orig( @args );
+
+    # Add the url_root
+    my $url_root = $self->url_root;
+    for my $page ( @pages ) {
+        next if $page->path =~ /^$url_root/;
+        $page->path( join "/", $url_root, $page->path );
+    }
+
+    return @pages;
+};
+
+=method url( $url )
+
+Get a URL to a page in this application. Prepends the L</url_root> if necessary. Strips
+"index.html" if possible.
+
+=cut
+
+sub url {
+    my ( $self, $url ) = @_;
+    $url =~ s{/index[.]html$}{};
+    return join "/", $self->url_root, $url;
+}
+
+=method link( %args )
+
+Create a link to a page in this application. C<%args> are attributes to be given to
+L<Statocles::Link> constructor.
+
+=cut
+
+sub link {
+    my ( $self, %args ) = @_;
+    my $url_root = $self->url_root;
+    if ( $args{href} !~ /^$url_root/ ) {
+        $args{href} = $self->url( $args{href} );
+    }
+    return Statocles::Link->new( %args );
+}
 
 1;
 __END__

lib/Statocles/App/Blog.pm
@@ -8,7 +8,7 @@ use Statocles::Page::Document;
 use Statocles::Page::List;
 use Statocles::Page::Feed;
 
-extends 'Statocles::App';
+with 'Statocles::App';
 
 =attr store
 
@@ -20,19 +20,6 @@ has store => (
     is => 'ro',
     isa => Store,
     coerce => Store->coercion,
-    required => 1,
-);
-
-=attr url_root
-
-The URL root of this application. All pages from this app will be under this
-root. Use this to ensure two apps do not try to write the same path.
-
-=cut
-
-has url_root => (
-    is => 'ro',
-    isa => Str,
     required => 1,
 );
 
@@ -198,7 +185,7 @@ sub post_pages {
     my $today = Time::Piece->new->ymd;
     my @pages;
     for my $doc ( @{ $self->store->documents } ) {
-        my $path = join "/", $self->url_root, $doc->path;
+        my $path = $doc->path;
         $path =~ s{/{2,}}{/}g;
         $path =~ s{[.]\w+$}{.html};
 
@@ -210,9 +197,9 @@ sub post_pages {
 
         my @tags;
         for my $tag ( @{ $doc->tags } ) {
-            push @tags, Statocles::Link->new(
+            push @tags, $self->link(
                 text => $tag,
-                href => $self->_tag_url( $tag ),
+                href => join( "/", 'tag', $self->_tag_url( $tag ) ),
             );
         }
 
@@ -271,8 +258,8 @@ sub index {
 
     my @pages = Statocles::Page::List->paginate(
         after => $self->page_size,
-        path => join( "/", $self->url_root, 'page/%i/index.html' ),
-        index => join( "/", $self->url_root, 'index.html' ),
+        path => 'page/%i/index.html',
+        index => 'index.html',
         # Sorting by path just happens to also sort by date
         pages => [ sort { $b->path cmp $a->path } @index_post_pages ],
         app => $self,
@@ -290,11 +277,11 @@ sub index {
             app => $self,
             type => $FEEDS{ $feed }{ type },
             page => $index,
-            path => join( "/", $self->url_root, 'index.' . $feed ),
+            path => 'index.' . $feed,
             template => $self->site->theme->template( blog => $FEEDS{$feed}{template} ),
         );
         push @feed_pages, $page;
-        push @feed_links, Statocles::Link->new(
+        push @feed_links, $self->link(
             text => $FEEDS{ $feed }{ text },
             href => $page->path->stringify,
             type => $page->type,
@@ -324,8 +311,8 @@ sub tag_pages {
     for my $tag ( keys %tagged_docs ) {
         my @tag_pages = Statocles::Page::List->paginate(
             after => $self->page_size,
-            path => join( "/", $self->url_root, 'tag', $tag, 'page/%i/index.html' ),
-            index => join( "/", $self->_tag_url( $tag ), 'index.html' ),
+            path => join( "/", 'tag', $self->_tag_url( $tag ), 'page/%i/index.html' ),
+            index => join( "/", 'tag', $self->_tag_url( $tag ), 'index.html' ),
             # Sorting by path just happens to also sort by date
             pages => [ sort { $b->path cmp $a->path } @{ $tagged_docs{ $tag } } ],
             app => $self,
@@ -337,18 +324,17 @@ sub tag_pages {
         my @feed_pages;
         my @feed_links;
         for my $feed ( sort keys %FEEDS ) {
-            my $tag_file = $tag . '.' . $feed;
-            $tag_file =~ s/\s+/-/g;
+            my $tag_file = $self->_tag_url( $tag ) . '.' . $feed;
 
             my $page = Statocles::Page::Feed->new(
                 type => $FEEDS{ $feed }{ type },
                 app => $self,
                 page => $index,
-                path => join( "/", $self->url_root, 'tag', $tag_file ),
+                path => join( "/", 'tag', $tag_file ),
                 template => $self->site->theme->template( blog => $FEEDS{$feed}{template} ),
             );
             push @feed_pages, $page;
-            push @feed_links, Statocles::Link->new(
+            push @feed_links, $self->link(
                 text => $FEEDS{ $feed }{ text },
                 href => $page->path->stringify,
                 type => $page->type,
@@ -394,7 +380,7 @@ tag links. The common attributes are:
 sub tags {
     my ( $self ) = @_;
     my %tagged_docs = $self->_tag_docs( @{ $self->_post_pages } );
-    return map {; Statocles::Link->new( text => $_, href => $self->_tag_url( $_ ) ) }
+    return map {; $self->link( text => $_, href => join( "/", 'tag', $self->_tag_url( $_ ) ) ) }
         sort keys %tagged_docs
 }
 
@@ -412,7 +398,7 @@ sub _tag_docs {
 sub _tag_url {
     my ( $self, $tag ) = @_;
     $tag =~ s/\s+/-/g;
-    return join "/", $self->url_root, "tag", $tag;
+    return $tag;
 }
 
 =method page_url( page )

lib/Statocles/App/Perldoc.pm
@@ -2,25 +2,12 @@ package Statocles::App::Perldoc;
 # ABSTRACT: Render documentation for Perl modules
 
 use Statocles::Base 'Class';
-extends 'Statocles::App';
 use Statocles::Page::Plain;
 use Scalar::Util qw( blessed );
 use List::MoreUtils qw( any );
 use Pod::Simple::Search;
 use Pod::Simple::XHTML;
-
-=attr url_root
-
-The URL root of this application. All pages from this app will be under this
-root. Use this to ensure two apps do not try to write the same path.
-
-=cut
-
-has url_root => (
-    is => 'ro',
-    isa => Str,
-    required => 1,
-);
+with 'Statocles::App';
 
 =attr inc
 
@@ -157,13 +144,13 @@ sub pages {
                     $new_href = join '/', split /::/, $href;
                     $new_href .= '.html';
                 }
-                $node->attr( href => join '/', $self->url_root, $new_href );
+                $node->attr( href => $self->url( $new_href ) );
             }
         }
 
         if ( $module eq $self->index_module ) {
             unshift @pages, Statocles::Page::Plain->new(
-                path => join( '/', $self->url_root, 'index.html' ),
+                path => join( '/', 'index.html' ),
                 layout => $self->site->theme->template( site => 'layout.html' ),
                 template => $self->site->theme->template( perldoc => 'pod.html' ),
                 content => "$dom",
@@ -175,7 +162,7 @@ sub pages {
             $page_url =~ s{::}{/}g;
 
             push @pages, Statocles::Page::Plain->new(
-                path => join( '/', $self->url_root, $page_url ),
+                path => join( '/', $page_url ),
                 layout => $self->site->theme->template( site => 'layout.html' ),
                 template => $self->site->theme->template( perldoc => 'pod.html' ),
                 content => "$dom",

lib/Statocles/App/Plain.pm
@@ -2,21 +2,9 @@ package Statocles::App::Plain;
 # ABSTRACT: Plain documents made into pages with no extras
 
 use Statocles::Base 'Class';
-extends 'Statocles::App';
 use List::Util qw( first );
 use Statocles::Page::Document;
-
-=attr url_root
-
-The root URL for this application. Required.
-
-=cut
-
-has url_root => (
-    is => 'ro',
-    isa => Str,
-    required => 1,
-);
+with 'Statocles::App';
 
 =attr store
 
@@ -47,7 +35,7 @@ sub pages {
 
         my $page = Statocles::Page::Document->new(
             app => $self,
-            path => join( '/', $self->url_root, $url ),
+            path => $url,
             document => $doc,
             layout => $self->site->theme->template( site => 'layout.html' ),
         );

lib/Statocles/App/Static.pm
@@ -2,20 +2,8 @@ package Statocles::App::Static;
 # ABSTRACT: Manage static files like CSS, JS, images, and other untemplated content
 
 use Statocles::Base 'Class';
-extends 'Statocles::App';
 use Statocles::Page::File;
-
-=attr url_root
-
-The root URL for this application. Required.
-
-=cut
-
-has url_root => (
-    is => 'ro',
-    isa => Str,
-    required => 1,
-);
+with 'Statocles::App';
 
 =attr store
 
@@ -51,7 +39,7 @@ sub pages {
         }
 
         push @pages, Statocles::Page::File->new(
-            path => join( "/", $self->url_root, $path ),
+            path => $path,
             fh => $self->store->open_file( $path ),
         );
     }

lib/Statocles/Page.pm
@@ -25,7 +25,7 @@ The application this page came from, so we can give it to the templates.
 
 has app => (
     is => 'ro',
-    isa => InstanceOf['Statocles::App'],
+    isa => ConsumerOf['Statocles::App'],
 );
 
 =attr path

lib/Statocles/Site.pm
@@ -59,7 +59,7 @@ that can be used later.
 
 has apps => (
     is => 'ro',
-    isa => HashRef[InstanceOf['Statocles::App']],
+    isa => HashRef[ConsumerOf['Statocles::App']],
     default => sub { {} },
 );
 

commit 03ce696cf0fbc322ee2740d97ef6bacd668818ab

fix pagination in the blog
This got broken in 32bfcd86 when we moved url_root into Statocles::App.

Fixes #304

lib/Statocles/App.pm
@@ -57,8 +57,17 @@ around pages => sub {
     # Add the url_root
     my $url_root = $self->url_root;
     for my $page ( @pages ) {
-        next if $page->path =~ /^$url_root/;
-        $page->path( join "/", $url_root, $page->path );
+        my @url_attrs = qw( path );
+
+        if ( $page->isa( 'Statocles::Page::List' ) ) {
+            push @url_attrs, qw( next prev );
+        }
+
+        for my $attr ( @url_attrs ) {
+            if ( $page->$attr && $page->$attr !~ /^$url_root/ ) {
+                $page->$attr( join "/", $url_root, $page->$attr );
+            }
+        }
     }
 
     return @pages;

lib/Statocles/App/Blog.pm
@@ -342,8 +342,8 @@ sub index {
 
     my @pages = Statocles::Page::List->paginate(
         after => $self->page_size,
-        path => 'page/%i/index.html',
-        index => 'index.html',
+        path => '/page/%i/index.html',
+        index => '/index.html',
         # Sorting by path just happens to also sort by date
         pages => [ sort { $b->path cmp $a->path } @index_post_pages ],
         app => $self,
@@ -395,8 +395,8 @@ sub tag_pages {
     for my $tag ( keys %tagged_docs ) {
         my @tag_pages = Statocles::Page::List->paginate(
             after => $self->page_size,
-            path => join( "/", 'tag', $self->_tag_url( $tag ), 'page/%i/index.html' ),
-            index => join( "/", 'tag', $self->_tag_url( $tag ), 'index.html' ),
+            path => join( "/", '', 'tag', $self->_tag_url( $tag ), 'page/%i/index.html' ),
+            index => join( "/", '', 'tag', $self->_tag_url( $tag ), 'index.html' ),
             # Sorting by path just happens to also sort by date
             pages => [ sort { $b->path cmp $a->path } @{ $tagged_docs{ $tag } } ],
             app => $self,

lib/Statocles/Page/List.pm
@@ -29,7 +29,7 @@ The path to the previous page in the pagination series.
 =cut
 
 has [qw( next prev )] => (
-    is => 'ro',
+    is => 'rw',
     isa => Path,
     coerce => Path->coercion,
 );

t/app/blog/pages.t
@@ -61,6 +61,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button button-primary';
+        is $prev->at( 'a' )->attr( 'href' ), '/blog/page/2';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $next->at( 'a' )->attr( 'href' ), '';
     },
 
     '/blog/page/2/index.html' => sub {
@@ -97,6 +105,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $prev->at( 'a' )->attr( 'href' ), '';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button button-primary';
+        is $next->at( 'a' )->attr( 'href' ), '/blog';
     },
 
     # Index feeds
@@ -200,6 +216,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button button-primary';
+        is $prev->at( 'a' )->attr( 'href' ), '/blog/tag/better/page/2';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $next->at( 'a' )->attr( 'href' ), '';
     },
 
     '/blog/tag/better/page/2/index.html' => sub {
@@ -236,6 +260,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $prev->at( 'a' )->attr( 'href' ), '';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button button-primary';
+        is $next->at( 'a' )->attr( 'href' ), '/blog/tag/better';
     },
 
     '/blog/tag/error-message/index.html' => sub {
@@ -274,6 +306,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $prev->at( 'a' )->attr( 'href' ), '';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $next->at( 'a' )->attr( 'href' ), '';
     },
 
     '/blog/tag/more/index.html' => sub {
@@ -308,6 +348,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $prev->at( 'a' )->attr( 'href' ), '';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $next->at( 'a' )->attr( 'href' ), '';
     },
 
     '/blog/tag/even-more-tags/index.html' => sub {
@@ -342,6 +390,14 @@ my @page_tests = (
         if ( ok my $node = $dom->at( 'footer #app-info' ) ) {
             is $node->text, $app->data->{info}, 'app-info is correct';
         }
+
+        my $prev = $dom->at( '.pager .prev' );
+        is $prev->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $prev->at( 'a' )->attr( 'href' ), '';
+
+        my $next = $dom->at( '.pager .next' );
+        is $next->at( 'a' )->attr( 'class' ), 'button disabled';
+        is $next->at( 'a' )->attr( 'href' ), '';
     },
 
     # Tag feeds

t/share/theme/blog/index.html.ep
@@ -21,6 +21,23 @@
     </article>
 % }
 
+<ul class="pager">
+    <li class="prev">
+        <a class="button <%= $self->next ? 'button-primary' : 'disabled' %>"
+            rel="next" href="<%= $self->next // '' %>"
+        >
+            &larr; Older
+        </a>
+    </li>
+    <li class="next">
+        <a class="button <%= $self->prev ? 'button-primary' : 'disabled' %>"
+            rel="prev" href="<%= $self->prev // '' %>"
+        >
+            Newer &rarr;
+        </a>
+    </li>
+</ul>
+
 <h1>Tags</h1>
 <ul class="list-inline tags">
 % for my $tag ( $app->tags ) {