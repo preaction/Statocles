344b56b13189b61d2100c75824a177264012718d
Doug Bell <madcityzen@gmail.com>
2015-09-08 14:16:48 -0500

    allow plain hashrefs in test app
    This allows us to write config files that contain test app pages

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -2,18 +2,20 @@ package
     TestApp;
 
 use Statocles::Base 'Class';
+use Scalar::Util qw( blessed );
+use Statocles::Page::Plain;
 with 'Statocles::App';
 
 has _pages => (
     is => 'ro',
-    isa => ArrayRef[ConsumerOf['Statocles::Page']],
+    isa => ArrayRef[ConsumerOf['Statocles::Page']|HashRef],
     init_arg => 'pages',
     default => sub { [] },
 );
 
 sub pages {
     my ( $self ) = @_;
-    return @{ $self->_pages };
+    return map { blessed $_ ? $_ : Statocles::Page::Plain->new( %$_ ) } @{ $self->_pages };
 }
 
 1;
