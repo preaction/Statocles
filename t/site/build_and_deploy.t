preaction/Statocles

commit 2c8d3d465f3e144a7b94236327a4cad0aa854e08

make Theme into an App
Previously, we added theme files to the site in a custom manner. This
prevents the Theme object from having any control over what files made
it into the site.

Now, the Theme object has control over what goes in, so it can decide
not to add the template files, for example.

Refs #399

lib/Statocles/Site.pm
@@ -449,14 +449,9 @@ sub build {
     $store->write_file( 'robots.txt', $robots->render );
 
     # Add the theme
-    my $theme_iter = $self->theme->store->find_files();
-    while ( my $theme_file = $theme_iter->() ) {
-        my $fh = $self->theme->store->open_file( $theme_file );
-        push @pages, Statocles::Page::File->new(
-            path => join( '/', '', 'theme', $theme_file ),
-            fh => $fh,
-        );
-        $store->write_file( Path::Tiny->new( 'theme', $theme_file ), $fh );
+    for my $page ( $self->theme->pages ) {
+        push @pages, $page;
+        $store->write_file( $page->path, $page );
     }
 
     $self->emit( build => class => 'Statocles::Event::Pages', pages => \@pages );

lib/Statocles/Theme.pm
@@ -5,6 +5,15 @@ use Statocles::Base 'Class';
 use File::Share qw( dist_dir );
 use Scalar::Util qw( blessed );
 use Statocles::Template;
+with 'Statocles::App::Role::Store';
+
+=attr url_root
+
+The root URL for this application. Defaults to C</theme>.
+
+=cut
+
+has '+url_root' => ( default => sub { '/theme' } );
 
 =attr store
 
@@ -14,13 +23,6 @@ If the path begins with ::, will pull one of the Statocles default
 themes from the Statocles share directory.
 
 =cut
-
-has store => (
-    is => 'ro',
-    isa => Store,
-    coerce => Store->coercion,
-    required => 1,
-);
 
 =attr include_stores
 

t/theme/pages.t
@@ -1,0 +1,30 @@
+
+use Statocles::Base 'Test';
+use Statocles::Theme;
+
+my $SHARE_DIR = path( __DIR__ )->parent->child( 'share' );
+my $site = build_test_site(
+    theme => $SHARE_DIR->child( 'theme' ),
+);
+
+my $app = Statocles::Theme->new(
+    #url_root => '/theme',      # Default to /theme
+    site => $site,
+    store => $SHARE_DIR->child( qw( theme ) ),
+);
+
+my %pages;
+my $iter = $SHARE_DIR->child( 'theme' )->iterator( { recurse => 1 } );
+while ( my $path = $iter->() ) {
+    next unless $path->is_file;
+    my $rel_path = $path->relative( $SHARE_DIR->child( 'theme' ) );
+    $pages{ "/theme/" . $rel_path } = sub {
+        my ( $output ) = @_;
+        eq_or_diff $output, $path->slurp_utf8, 'Theme file is correct: ' . $rel_path;
+    };
+}
+
+test_pages( $site, $app, %pages );
+
+done_testing;
+

commit fb192c8e6d0b937db4bc47cfbaa763772c706395

correctly render theme include files
This was a regression from 2c8d3d465f (#399): The theme files were
stringified page objects, not rendered page objects like they were
supposed to be. The tests were only checking for existence, not content,
so this was not caught.

Fixes #402

lib/Statocles/Site.pm
@@ -451,7 +451,7 @@ sub build {
     # Add the theme
     for my $page ( $self->theme->pages ) {
         push @pages, $page;
-        $store->write_file( $page->path, $page );
+        $store->write_file( $page->path, $page->render( %args ) );
     }
 
     $self->emit( build => class => 'Statocles::Event::Pages', pages => \@pages );

t/share/theme/css/normalize.css
@@ -1,0 +1,4 @@
+* {
+    margin: 0;
+    padding: 0;
+}

t/site/build_and_deploy.t
@@ -56,7 +56,11 @@ subtest 'build' => sub {
             next if $theme_file =~ /[.]ep$/;
             my $path = path( 'theme' => $theme_file );
             ok $build_dir->child( $path )->exists,
-                'theme file ' . $theme_file . 'exists in build dir';
+                'theme file ' . $theme_file . ' exists in build dir';
+            eq_or_diff
+                $build_dir->child( $path )->slurp_utf8,
+                $site->theme->store->path->child( $theme_file )->slurp_utf8,
+                'theme file ' . $theme_file . ' content is correct';
             ok !$deploy_dir->child( $path )->exists,
                 'theme file ' . $theme_file . 'not in deploy dir';
             push @pages, $path;
@@ -88,6 +92,10 @@ subtest 'deploy' => sub {
             next if $theme_file =~ /[.]ep$/;
             ok $deploy_dir->child( 'theme', $theme_file )->exists,
                 'theme file ' . $theme_file . 'exists in deploy dir';
+            eq_or_diff
+                $build_dir->child( theme => $theme_file )->slurp_utf8,
+                $site->theme->store->path->child( $theme_file )->slurp_utf8,
+                'theme file ' . $theme_file . ' content is correct';
         }
     };
 