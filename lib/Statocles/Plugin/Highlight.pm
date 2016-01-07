preaction/Statocles

commit 6564e6c28b5a513e96fc7957af3f1741d845b7a7

fix highlight plugin to work with begin/end
Mojo::Template's `begin`/`end` gives the calling function a subref to
call to get the text, so we have to make sure we try to see if the text
is actually a subref.

lib/Statocles/Plugin/Highlight.pm
@@ -134,6 +134,13 @@ my $hl = Syntax::Highlight::Engine::Kate->new(
 sub highlight {
     my ( $self, $args, $type, $text ) = @_;
 
+    # Handle Mojolicious begin/end
+    if ( ref $text eq 'CODE' ) {
+        $text = $text->();
+        # This adds an extra newline to the first line, so remove it
+        $text =~ s/^\n//;
+    }
+
     # XXX We need to normalize this so that the current page is always
     # `$args->{page}`
     my $page = $args->{page} || $args->{self};

t/plugin/highlight.t
@@ -73,10 +73,30 @@ subtest 'register' => sub {
 
 subtest 'test helper interaction' => sub {
 
-    subtest 'highlight an included file' => sub {
-        my $plugin = Statocles::Plugin::Highlight->new(
-            style => 'solarized-dark',
+    subtest 'begin/end' => sub {
+        my $plugin = Statocles::Plugin::Highlight->new;
+
+        my $site = build_test_site(
+            theme => $SHARE_DIR->child( 'theme' ),
+            plugins => {
+                highlight => $plugin,
+            }
         );
+
+        my $tmpl = $site->theme->build_template(
+            test => <<ENDTMPL,
+<%= highlight html => begin %><h1>Title</h1><% end %>
+ENDTMPL
+        );
+
+        is $tmpl->render,
+            qq{<span class="hljs-keyword">&lt;h1&gt;</span>Title<span class="hljs-keyword">&lt;/h1&gt;</span>\n},
+            'highlight works with begin/end';
+
+    };
+
+    subtest 'highlight an included file' => sub {
+        my $plugin = Statocles::Plugin::Highlight->new;
 
         my $site = build_test_site(
             theme => $SHARE_DIR->child( 'theme' ),

commit adad407c89eb51540d97b3a65612acfb51e1e84d

fix too much space on the top of highlight blocks
Using `begin`/`end` adds a newline to the top of the code, which doesn't
look good when we then wrap it in a `<pre>`.

This is a regression from 6564e6c28b5a513e96fc7957af3f1741d845b7a7

Fixes #431

lib/Statocles/Plugin/Highlight.pm
@@ -131,6 +131,9 @@ sub highlight {
     # Handle Mojolicious begin/end
     if ( ref $text eq 'CODE' ) {
         $text = $text->();
+        # begin/end starts with a newline, so remove it to prevent too
+        # much top space
+        $text =~ s/^\n//;
     }
 
     # XXX We need to normalize this so that the current page is always

t/plugin/highlight.t
@@ -92,12 +92,14 @@ subtest 'test helper interaction' => sub {
 
         my $tmpl = $site->theme->build_template(
             test => <<ENDTMPL,
-<%= highlight html => begin %><h1>Title</h1><% end %>
+<%= highlight html => begin %>
+<h1>Title</h1>
+<% end %>
 ENDTMPL
         );
 
         eq_or_diff $tmpl->render,
-            qq{<pre><code class="hljs"><span class="hljs-keyword">&lt;h1&gt;</span>Title<span class="hljs-keyword">&lt;/h1&gt;</span></code></pre>\n},
+            qq{<pre><code class="hljs"><span class="hljs-keyword">&lt;h1&gt;</span>Title<span class="hljs-keyword">&lt;/h1&gt;</span>\n</code></pre>\n},
             'highlight works with begin/end';
 
     };
@@ -113,7 +115,7 @@ ENDTMPL
         );
 
         my $tmpl = $site->theme->build_template(
-            test => '<%= highlight html => begin %><%= include "include/test.markdown.ep" %><%= end %>',
+            test => '<%= highlight html => include "include/test.markdown.ep" %>',
         );
         eq_or_diff $tmpl->render( title => "Title" ),
             qq{<pre><code class="hljs"><span class="hljs-keyword">&lt;h1&gt;</span>Title<span class="hljs-keyword">&lt;/h1&gt;</span>\n</code></pre>\n},