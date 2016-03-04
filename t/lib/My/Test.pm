0bb9ed55aeb99d0f528ff7f67b4909009c422b05
Kent Fredric <kentfredric@gmail.com>
2016-03-04 17:03:55 +1300

    Copy test_constructor to lib/My/Test.pm

--- t/lib/My/Test.pm
+++ t/lib/My/Test.pm
@@ -4,6 +4,7 @@ package My::Test;
 
 use strict;
 use warnings;
+
 use base 'Import::Base';
 
 our @IMPORT_MODULES = (
@@ -23,13 +24,88 @@ our @IMPORT_MODULES = (
     'Dir::Self' => [qw( __DIR__ )],
     'Path::Tiny' => [qw( path tempdir cwd )],
     'Statocles::Test' => [qw(
-      test_constructor test_pages build_test_site build_test_site_apps
+      test_pages build_test_site build_test_site_apps
       build_temp_site
     )],
     'Statocles::Types' => [qw( DateTimeObj )],
+    'My::Test::_Extras' => [qw( test_constructor )],
     sub { $Statocles::VERSION ||= 0.001; return }, # Set version normally done via dzil
 );
 
+package My::Test::_Extras;
 
+$INC{'My/Test/_Extras.pm'} = 1;
+
+require Exporter;
+*import = \&Exporter::import;
+
+our @EXPORT_OK = qw( test_constructor );
+
+sub test_constructor {
+    my ( $class, %args ) = @_;
+
+    my %required = $args{required} ? ( %{ $args{required} } ) : ();
+    my %defaults = $args{default}  ? ( %{ $args{default} } )  : ();
+    require Test::Builder;
+    require Scalar::Util;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    my $tb = Test::Builder->new();
+
+    $tb->subtest(
+        $class . ' constructor' => sub {
+            my $got    = $class->new(%required);
+            my $want   = $class;
+            my $typeof = do {
+                    !defined $got                ? 'undefined'
+                  : !ref $got                    ? 'scalar'
+                  : !Scalar::Util::blessed($got) ? ref $got
+                  : eval { $got->isa($want) } ? $want
+                  :                             Scalar::Util::blessed($got);
+            };
+            $tb->is_eq( $typeof, $class,
+                'constructor works with all required args' );
+
+            if ( $args{required} ) {
+                $tb->subtest(
+                    'required attributes' => sub {
+                        for my $key ( keys %required ) {
+                            require Test::Exception;
+                            &Test::Exception::dies_ok(
+                                sub {
+                                    $class->new(
+                                        map { ; $_ => $required{$_} }
+                                        grep { $_ ne $key } keys %required,
+                                    );
+                                },
+                                $key . ' is required'
+                            );
+                        }
+                    }
+                );
+            }
+
+            if ( $args{default} ) {
+                $tb->subtest(
+                    'attribute defaults' => sub {
+                        my $obj = $class->new(%required);
+                        for my $key ( keys %defaults ) {
+                            if ( ref $defaults{$key} eq 'CODE' ) {
+                                local $_ = $obj->$key;
+                                $tb->subtest(
+                                    "$key default value" => $defaults{$key} );
+                            }
+                            else {
+                                require Test::Deep;
+                                Test::Deep::cmp_deeply( $obj->$key,
+                                    $defaults{$key}, "$key default value" );
+                            }
+                        }
+                    }
+                );
+            }
+
+        }
+    );
+}
 1;
-
